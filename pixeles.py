b4d66c9c8816e4b099c4f9811255f127fd5e0692
Holber Berdugo <holberjoelberdugo@gmail.com>
2023-02-22 08:56:48 -0500

    Le amentamos el contraste a la imagen y agregamos una fila mas al axis

--- pixeles.py
+++ pixeles.py
@@ -1,7 +1,7 @@
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('si2.jpg',0)
+img = cv2.imread('yeah.jpg',0)
 img2 = img.copy()
 
 (row, col) = img.shape
@@ -13,11 +13,19 @@ for i in range(row):
         if (img[i][j] > 240):
             img2[i][j] = 240
 
-fig, axs = plt.subplots(nrows = 1, ncols = 2, figsize =(8,8))
-axs[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-axs[0].axis("off")
+f_max= img2.max()
+f_min= img2.min()
 
-axs[1].imshow(img2, cmap="Greys")
-axs[1].axis("off")
+img3= img2.copy()
+for i in range (row):
+    for j in range(col):
+        img3[i][j]=((img2[i][j]-f_min)/(f_max-f_min))*256
+
+fig, axs = plt.subplots(nrows = 2, ncols = 2, figsize =(8,8))
+axs[0][0].imshow(img, cmap="gray")
+axs[0][0].axis("off")
+
+axs[1][0].imshow(img3, cmap="gray")
+axs[1][0].axis("off")
 
 plt.show()
