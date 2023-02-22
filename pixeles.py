43b73da942d8bb8b8925a29f914cf5388cfe95a2
Holber Berdugo <holberjoelberdugo@gmail.com>
2023-02-22 08:01:17 -0500

    Mi primer commit

---
+++ pixeles.py
@@ -0,0 +1,23 @@
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('si.jpeg',0)
+img2 = img.copy()
+
+(row, col) = img.shape
+
+for i in range(row):
+    for j in range(col):
+        if (img[i][j] < 10):
+            img2[i][j] = 10
+        if (img[i][j] > 240):
+            img2[i][j] = 240
+
+fig, axs = plt.subplots(nrows = 1, ncols = 2, figsize =(8,8))
+axs[0].imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+axs[0].axis("off")
+
+axs[1].imshow(img2, cmap="Greys")
+axs[1].axis("off")
+
+plt.show()
